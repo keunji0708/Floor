996a71f73dfd36332de41917584936c13767de51
keunji0708 <33439964+keunji0708@users.noreply.github.com>
2019-01-02 14:54:31 +0900

    Update floor.java
    Add a Main Loop & print input and output

--- Floor/src/floor.java
+++ Floor/src/floor.java
@@ -1,4 +1,11 @@
+import java.util.Scanner;
 
 public class floor {
-
+  public static void main(String args[]){
+    System.out.print("정수 N을 입력 : ");
+    Scanner input = new Scanner(System.in);
+    long n = input.nextLong();
+    
+    long result = 0;
+    System.out.println("floor값: " + result);
 }
