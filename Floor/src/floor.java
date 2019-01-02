23627b6ca196bcc502144d947c6245411a334c5b
keunji0708 <33439964+keunji0708@users.noreply.github.com>
2019-01-02 15:10:28 +0900

    Update floor.java
    Add log_floor method

--- Floor/src/floor.java
+++ Floor/src/floor.java
@@ -6,6 +6,18 @@ public class floor {
     Scanner input = new Scanner(System.in);
     long n = input.nextLong();
     
-    long result = 0;
+    long result = log_floor(n);
     System.out.println("floor값: " + result);
+  }
+  
+  public static long log_floor(long n) {
+    long e = -1;
+    long k = 1;
+    
+    while (k <= n) {
+      e = e + 1;
+      k = k * 2;
+    }
+     return e;
+  }
 }
