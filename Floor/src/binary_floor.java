a67ff111aaa7bcd3a3e35b133c453a6a0f75a22b
keunji0708 <33439964+keunji0708@users.noreply.github.com>
2019-01-03 15:46:18 +0900

    Update binary_floor.java

--- Floor/src/binary_floor.java
+++ Floor/src/binary_floor.java
@@ -1,24 +1,30 @@
 import java.util.Scanner;
 
 public class binary_floor {
-  public static long log_floor(long n){
-    long e = -1;
-    long k = 1;
-    
-    while (k <= n) {
-		      e = e + 1;
-		      k = k * 2;
-		    }
-		     return e;
-  }
-  
-  public static void main(String args[]{
-    System.out.print("정수 N을 입력 : ");
-    Scanner input = new Scanner(System.in);
-    long n = input.nextLong();
-    
-    long result = log_floor(n);
-    System.out.println("floor값: "+result);
-  }
-                         
+	public static long bi_log_floor(long n) {
+		long e = 0;
+		long k = 2;
+		long count = 0;
+		
+		while (k <= n){
+			if (e == 0){
+				e = e + 1;
+			} else {
+				e = e * 2;
+			}
+			k = k * k;
+			count = count + 1;
+		}
+		return 0;
+	}
+
+	public static void main(String args[]) {
+		System.out.print("정수 N을 입력 : ");
+		Scanner input = new Scanner(System.in);
+		long n = input.nextLong();
+		
+		long bi_result = bi_log_floor(n);
+		
+		System.out.println("이진 탐색 floor 값 : " + bi_result);
+	}
 }
